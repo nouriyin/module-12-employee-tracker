b962ac98252ebec854b36134cb4508f6026f96ad
nouriyin <courtneyy.noreen@gmail.com>
2023-05-31 20:41:49 -0400

    added seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -21,8 +21,8 @@ CREATE TABLE role (
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30),
-    role_id INT,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
     manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
